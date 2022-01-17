689f6d7f62b159c4f68170e2726324847736bcb2
labadcloyd <abadcloyd@gmail.com>
2022-01-17 11:31:02 +0800

    setting up ingress

--- backend/auth/src/index.ts
+++ backend/auth/src/index.ts
@@ -4,6 +4,14 @@ import { json } from "body-parser";
 const app = express()
 app.use(json())
 
+app.get('/api/users/currentuser', (req, res) =>{
+	console.log(req)
+	return res.status(200).json({
+		name: 'Cloyd',
+		username: 'labadcloyd',
+	})
+})
+
 app.listen(3000, () => {
 	console.log('running on port 3000')
 })
