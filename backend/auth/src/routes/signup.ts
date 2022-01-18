58584e5c75adbc4b09d298508ed1b3482d1d8bd4
labadcloyd <abadcloyd@gmail.com>
2022-01-18 13:38:21 +0800

    added validation to signup route

--- backend/auth/src/routes/signup.ts
+++ backend/auth/src/routes/signup.ts
@@ -1,8 +1,29 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
 
 const app = express.Router()
 
-app.post('/api/users/signup', (req, res) =>{
+app.post('/api/users/signup', [
+	body('email')
+		.isEmail()
+		.withMessage('Must be a valid email'),
+	body('password')
+		.trim()
+		.isLength({ min: 7, max: 20 })
+		.withMessage('Password must be between 7 and 20 characters')
+], (req: Request, res: Response) => {
+	const errors = 	validationResult(req)
+
+	if (!errors.isEmpty()) {
+		return res.status(400).send({ message: 'Invalid Input' })
+	}
+
+	const { email, password } = req.body
+
+	console.log(email, password)
+
+	return res.status(200).json({ message: 'Successfully signed up' })
+
 })
 
 export { app as signupRouter }
