84be95419822eeefafdf4241649c74bb9006c40b
labadcloyd <abadcloyd@gmail.com>
2022-01-22 12:10:49 +0800

    improved user model for typescript usability

--- backend/auth/src/models/user.ts
+++ backend/auth/src/models/user.ts
@@ -1,8 +1,20 @@
-import { Schema, model } from 'mongoose'
+import { Schema, model, Model, Document as MongoDocument } from 'mongoose'
 
-interface UserAttrs {
+/* TYPESCRIPT BOILERPLATE */
+// interface that describes the required fields to be entered to create a new model
+interface ModelAttrs {
 	email: string,
 	password: string,
+}
+// interface that describes the properties of a single mongo document
+interface MongoDoc extends MongoDocument {
+	email: string,
+	password: string,
+}
+// interface that tells typescript about the new function added to user model
+// "Model" is a built in typescript interface and not an actual mongoose object
+interface MongoModel extends Model<MongoDoc> {
+	build(attrs: ModelAttrs): any;
 }
 
 const UserSchema = new Schema({
@@ -16,10 +28,10 @@ const UserSchema = new Schema({
 	}
 })
 
-const User = model('User', UserSchema)
-
-const newUser = (attrs: UserAttrs) => {
+UserSchema.statics.build = (attrs: ModelAttrs) => {
 	return new User(attrs)
 }
 
-export { newUser }
+const User = model<MongoDoc, MongoModel>('User', UserSchema)
+
+export { User }
