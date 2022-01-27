db4da33963600c56fa59d58aaad0aebba97c62fb
labadcloyd <abadcloyd@gmail.com>
2022-01-27 18:43:41 +0800

    fixing errors on jest and writing first test

--- backend/auth/src/test/setup.ts
+++ backend/auth/src/test/setup.ts
@@ -5,8 +5,10 @@ import { app } from '../app'
 let mongo: any
 
 beforeAll(async () => {
-	mongo = new MongoMemoryServer()
-	const mongoUri = await mongo.getUri()
+	process.env.JWTSECRET = 'asdasdasd123'
+
+	mongo = await MongoMemoryServer.create()
+	const mongoUri = mongo.getUri()
 
 	await mongoose.connect(mongoUri)
 })
