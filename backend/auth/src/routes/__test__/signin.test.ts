5a66990f106c564a7900f9488dcea5b58666a5bf
labadcloyd <abadcloyd@gmail.com>
2022-01-28 10:48:36 +0800

    added error testing on signin route

--- backend/auth/src/routes/__test__/signin.test.ts
+++ backend/auth/src/routes/__test__/signin.test.ts
@@ -1,6 +1,60 @@
 import request from 'supertest'
 import { app } from '../../app'
 
+it('returns a 400 on wrong password', async () => {
+  await request(app)
+		.post('/api/users/signup')
+		.send({
+			email: 'test@test.com',
+			password: 'password'
+		})
+		.expect(201);
+
+	return await request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test@test.com',
+      password: 'password123'
+    })
+    .expect(400)
+});
+
+it('returns a 400 on wrong email', async () => {
+  await request(app)
+		.post('/api/users/signup')
+		.send({
+			email: 'test@test.com',
+			password: 'password'
+		})
+		.expect(201);
+
+	return await request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test1@test.com',
+      password: 'password'
+    })
+    .expect(400)
+});
+
+it('returns a 400 on wrong email AND password', async () => {
+  await request(app)
+		.post('/api/users/signup')
+		.send({
+			email: 'test@test.com',
+			password: 'password'
+		})
+		.expect(201);
+
+	return await request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test1@test.com',
+      password: 'password1'
+    })
+    .expect(400)
+});
+
 it('returns a 201 on successful signin', async () => {
   await request(app)
 		.post('/api/users/signup')
