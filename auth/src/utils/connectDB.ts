5e3996a92ce60792da19d0b4ab44ed663abfa403
labadcloyd <abadcloyd@gmail.com>
2022-02-03 19:19:35 +0800

    updated mongodb uri for connecting

--- auth/src/utils/connectDB.ts
+++ auth/src/utils/connectDB.ts
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 import { DatabaseConnectionError } from "@ticketlabs/common";
 
 const connectDB = async () => {
-	if (!process.env.JWTSECRET) {
-		throw new Error('JWTSECRET must be defined')
+	if (!process.env.JWTSECRET || !process.env.MONGO_URI) {
+		throw new Error('JWTSECRET or MONGO_URI must be defined')
 	}
 	try {
-		await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
+		await mongoose.connect(process.env.MONGO_URI)
 		console.log('Database connected')
 	} catch(err) {
 		console.log(err)
