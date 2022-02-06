8fc7ba2331c23d527da1b78c23349ce75dd03432
labadcloyd <abadcloyd@gmail.com>
2022-02-06 20:56:23 +0800

    added ticket model

---
+++ tickets/src/models/ticket.ts
@@ -0,0 +1,56 @@
+import { Schema, model, Model, Document as MongoDocument, ObjectId } from 'mongoose'
+
+/* TYPESCRIPT BOILERPLATE */
+// interface that describes the required fields to be entered to create a new model
+interface ModelAttrs {
+	title: string,
+	price: number,
+	userId: string,
+}
+// interface that describes the properties of a single mongo document
+interface MongoDoc extends MongoDocument {
+	title: string,
+	price: number,
+	userId: string,
+	_id: ObjectId,
+}
+// interface that tells typescript about the new function added to ticket model
+// "Model" is a built in typescript interface and not an actual mongoose object
+interface MongoModel extends Model<MongoDoc> {
+	build(attrs: ModelAttrs): MongoDoc;
+}
+
+const TicketSchema = new Schema({
+	title: {
+		type: String,
+		required: true
+	},
+	price: {
+		type: Number,
+		required: true
+	},
+	userId: {
+		type: String,
+		required: true
+	}
+},
+// Changing how mongoose will return the object once it is sent over to the client
+{
+	toJSON: {
+		transform(doc, ret){
+			ret.id = ret._id
+			delete ret._id
+			delete ret.__v
+		}
+	}
+}
+
+)
+
+TicketSchema.statics.build = (attrs: ModelAttrs) => {
+	return new Ticket(attrs)
+}
+
+const Ticket = model<MongoDoc, MongoModel>('Ticket', TicketSchema)
+
+export { Ticket }
--- tickets/src/routes/__test__/createTicket.test.ts
+++ tickets/src/routes/__test__/createTicket.test.ts
@@ -68,4 +68,17 @@ it('returns an error if an invalid price is provided', async () => {
 		.expect(400)
 });
 
-it('creates a ticket with valid inputs', async () => {});
+it('creates a ticket with valid inputs', async () => {
+	// add in a check to make sure a ticket was saved in the db
+
+	const cookie = global.signin()
+
+	await request(app)
+		.post('/api/tickets')
+		.set('Cookie', cookie)
+		.send({
+			title: 'Valid Title',
+			price: 20
+		})
+		.expect(201)
+});
