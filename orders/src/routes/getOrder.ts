6e45bc0f16f38fa07ea9a1c455467e4558361b57
labadcloyd <abadcloyd@gmail.com>
2022-03-01 11:25:04 +0800

    finished get specific order route

--- orders/src/routes/getOrder.ts
+++ orders/src/routes/getOrder.ts
@@ -1,21 +1,24 @@
 import express, { Request, Response } from "express";
-import { NotFoundError, requireAuth, validateRequest } from '@ticketlabs/common'
+import { NotFoundError, requireAuth, UnautherizedError, validateRequest } from '@ticketlabs/common'
 import { body, param, } from "express-validator";
 import { Order } from '../models'
 
 const app = express.Router()
 
-app.get('/api/orders/:id', [
-	param('id').isString().not().isEmpty().withMessage('Must be a valid id')
+app.get('/api/orders/:orderId', requireAuth, [
+	param('orderId').isString().not().isEmpty().withMessage('Must be a valid id')
 ], 
 validateRequest, async (req: Request, res: Response) => {
-	const { id } = req.params
+	const { orderId } = req.params
 
 	try {
-		const foundOrder = await Order.findById(id)
+		const foundOrder = await Order.findById(orderId).populate('ticket')
 
 		if (!foundOrder) {
 			throw new NotFoundError()
+		}
+		if (foundOrder.userId !== req.currentUser!.id) {
+			throw new UnautherizedError
 		}
 
 		return res.status(200).json(foundOrder)
