3dcecc596b79cb5373a29dc41ec6e32838884d5e
labadcloyd <abadcloyd@gmail.com>
2022-03-02 11:17:28 +0800

    updated tests for orders service

--- orders/src/routes/__test__/cancelOrder.test.ts
+++ orders/src/routes/__test__/cancelOrder.test.ts
@@ -3,6 +3,7 @@ import { app } from '../../app'
 import mongoose from 'mongoose'
 import { Order, Ticket } from '../../models';
 import { OrderStatus } from '@ticketlabs/common';
+import { natsWrapper } from '../../natsWrapper'
 
 it('marks an order as cancelled', async () => {
 	const ticket = Ticket.build({
@@ -28,4 +29,28 @@ it('marks an order as cancelled', async () => {
 	expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled)
 });
 
-it.todo('emits an order cancelled event')
+it('emits an order cancelled event', async () => {
+	const ticket = Ticket.build({
+		title: 'asda',
+		price: 20
+	})
+	await ticket.save()
+
+	const user = global.signin()
+
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+	
+	await request(app)
+		.delete(`/api/orders/${order.id}`)
+		.set('Cookie', user)
+		.expect(204)
+
+	const updatedOrder = await Order.findById(order.id)
+	expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled)
+
+	expect(natsWrapper.client.publish).toHaveBeenCalled()
+})
